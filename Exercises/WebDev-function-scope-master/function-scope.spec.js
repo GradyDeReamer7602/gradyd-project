bba29f32bd6105c5c6dfe9df029eea6bd01dfac3
GradyDeReamer7602 <50172539+GradyDeReamer7602@users.noreply.github.com>
2019-05-08 16:39:46 -0400

    e

---
+++ Exercises/WebDev-function-scope-master/function-scope.spec.js
@@ -0,0 +1,15 @@
+describe('Problem #1', function(){
+   it('answer is correct', function(){
+      expect(answerOneFunc()).to.equal('correct');
+   })
+});
+describe('Problem #2', function(){
+   it('answer is correct', function(){
+      expect(answerTwoFunc()).to.equal('correct');
+   })
+});
+describe('Problem #3', function(){
+   it('answer is correct', function(){
+      expect(answerThreeFunc()).to.equal('correct');
+   })
+});
